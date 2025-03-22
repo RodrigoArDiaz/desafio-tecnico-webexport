b8bb5be4e33510d42b730135fb1f12762e074652
Diaz <diazrodrigoar@gmail.com>
2025-03-22 09:45:09 -0300

    feat: agregar estructura base de vistas
    Agregar cdn de Bootstrap.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio tecnico WebExport</title>
+    <!-- Bootstrap CSS desde jsDelivr -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    {{-- @auth --}}
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body px-2" data-bs-theme="dark">
+            <a class="navbar-brand" href="{{ route('inicio')}}">App</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    {{-- <li class="nav-item">
+                        <a class="nav-link" href="{{ route('usuarios.create') }}">CRUD Usuarios</a>
+                    </li> --}}
+                    {{-- 
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Cerrar Sesión</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                    </li> --}}
+                </ul>
+            </div>
+        </nav>
+    {{-- @endauth --}}
+
+    {{-- Contenedor central --}}
+    <div class="container mt-4 mb-4">
+        @yield('content')
+    </div>
+
+    {{-- Scripts Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('inicio');
+
