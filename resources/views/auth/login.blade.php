105e55d9bed4d58c6f17da06b8331b4f9beed939
Diaz Rodrigo Armando <diazrodrigoar@gmail.com>
2025-03-23 02:05:14 -0300

    feat: mejorar diseño y estructura del formulario de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,31 +4,41 @@
 <div class="row justify-content-center">
     <div class="container mt-5">
         <div class="row justify-content-center">
-            <div class="col-md-6">
+            <div class="col-xl-4 col-lg-5 col-md-7 col-sm-9">
                 <div class="card">
-                    <div class="card-header">Iniciar Sesión</div>
+                    <div class="card-header bg-dark text-white text-center fs-4">
+                       ¡Bienvenido!
+                    </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-                            <div class="mb-3">
-                                <label for="mail" class="form-label">Correo Electrónico</label>
-                                <input type="email" class="form-control" id="mail" name="mail" required>
-                            </div>
-                            <div class="mb-3">
+                            <div class="mx-3 my-5">
+                                <div class="mb-3">
+                                    <label for="mail" class="form-label">Correo Electrónico</label>
+                                    <input type="email" class="form-control" id="mail" name="mail" required>
+                                </div>
                                 <label for="contrasenia" class="form-label">Contraseña</label>
-                                <input type="password" class="form-control" id="contrasenia" name="contrasenia" required>
+                                <div class="input-group mb-3">
+                                    <input type="password" class="form-control" id="contrasenia" name="contrasenia" required>
+                                    <button class="btn btn-outline-secondary" type="button" id="togglePassword">
+                                        <i class="bi bi-eye"></i> <!-- Ícono de ojo (usando Bootstrap Icons) -->
+                                    </button>
+                                </div>
                             </div>
-                            <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+
+                            <div class="d-flex justify-content-center align-items-center px-3 my-5">
+                                <button type="submit" class="btn btn-dark w-100">Iniciar Sesión</button>
+                            </div>
 
                             @if ($errors->any())
-                            <div class="alert alert-danger mt-3">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
+                                <div class="alert alert-danger m-3">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
                         </form>
                     </div>
                 </div>
@@ -36,4 +46,25 @@
         </div>
     </div>
 </div>
+
+
+<script>
+    const addEventTogglePassword = (idButtonToggle, idInput) => {
+        document.getElementById(idButtonToggle).addEventListener('click', function () {
+            const passwordInput = document.getElementById(idInput);
+            const icon = this.querySelector('i');
+
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                icon.classList.remove('bi-eye');
+                icon.classList.add('bi-eye-slash');
+            } else {
+                passwordInput.type = 'password';
+                icon.classList.remove('bi-eye-slash');
+                icon.classList.add('bi-eye');
+            }
+        });
+    }
+    addEventTogglePassword('togglePassword', 'contrasenia');
+</script>
 @endsection
